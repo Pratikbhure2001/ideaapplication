92210eb910ce068c1a06736792c2b8eed97b4fb7
Pratik Bhure <pratikbhure653@gmail.com>
2023-07-04 11:27:49 +0530

    added serverConfig

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express')
+const serverConfig=require('./config/server.config')
 const app=express();
-app.listen(2333,()=>{
-    console.log("server started")
+app.listen(serverConfig.PORT,()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}`)
 
 })
